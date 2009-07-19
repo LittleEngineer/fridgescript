f4d922d003860a33347e46c329d2cfa44f6bca57
jheriko <jheriko@78b37d4e-3566-11de-a9fe-ad1e22fc3a8a>
2009-07-19 12:02:54 +0000

    Split out MS compiler specific part of FSCompiledCode into Platform_FSCompiledCode.cpp

--- trunk/src/Independent/Code/FSCompiledCode.cpp
+++ trunk/src/Independent/Code/FSCompiledCode.cpp
@@ -20,62 +20,6 @@
 #include <Code/FSCompiledCode.h>
 #include <Variable/FSVariable.h>
 
-void FSCompiledCode::operator ()()
-{
-    FSAssert( bytes != 0, "Trying to execute code before it has been compiled!" );
-    
-    // need to set up the stack etc...
-    // this is a really crappy solution
-    static double crap[4096];
-    double* p = &(crap[0]);
-    unsigned int i;
-    void(__stdcall *f)() = reinterpret_cast<void(__stdcall *)()>(bytes);
-
-    for(i = 0; i < vars.GetCount(); ++i)
-    {
-        crap[i] = vars[i]->_auto;
-    }
-
-    __asm
-    {
-        mov eax, p
-        mov ecx, i
-lbl1:
-        // copy stuff
-        push dword ptr [eax+4]
-        push dword ptr [eax]
-        add eax, 8
-        loop lbl1
-
-        // store the pointer ready for copy back
-        mov p, eax
-        // load function address
-        mov eax, f
-
-        // setup stack frame and execute code
-        push ebp
-        mov ebp, esp
-        add ebp, 4
-        call eax                        // reinterpret_cast<void(__stdcall *)()>(bytes)();
-        pop ebp
-        
-        // load pointer, counter
-        mov eax, p
-        mov ecx, i
-lbl2:
-        // copy stuff
-        sub eax, 8
-        pop dword ptr [eax]
-        pop dword ptr [eax+4]
-        loop lbl2
-    }
-
-    for(i = 0; i < vars.GetCount(); ++i)
-    {
-        vars[i]->_auto = crap[i];
-    }
-}
-
 void FSCompiledCode::SetupVariableStack(Simple::Stack<FSVariable*>* v)
 {
     for(unsigned int i = 0; i < v->GetCount(); ++i)
